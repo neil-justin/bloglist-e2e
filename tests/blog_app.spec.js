0699bfa20fe45e704ddb008c5b77e670c1226d34
Neil <mallarineil9@gmail.com>
2024-07-23 17:49:17 +0800

    Write tests for failing and working logins

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,15 +1,37 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
-  test('Login form is shown', async ({ page }) => {
-    await page.getByTestId('username-input').fill('mluukkai')
-    await page.getByTestId('password-input').fill('salainen')
-    await page.getByRole('button', { name: 'login' }).click()
+  // write a test here for when a login form is shown
 
-    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username-input').fill('mluukkai')
+      await page.getByTestId('password-input').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username-input').fill('mluukkai')
+      await page.getByTestId('password-input').fill('wrongPassword')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByTestId('notification-element'))
+        .toContainText('wrong username or password')
+    })
   })
 })
